c6d8207c85728db9371da7eb7015d2434aeb01fb
color_box <majima@merge2.local>
2015-07-13 21:02:13 +0900

    first commit

---
+++ NavigationTrial/NavigationTrial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavigationTrial
+//
+//  Created by merge on 2015/07/13.
+//  Copyright (c) 2015年 merge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
